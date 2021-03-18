2aca88a4acb687d554a64fc8fff5964cf8825a71
Robert Chaing <rchaing@figure.com>
2021-03-17 17:52:21 -0700

    pass cryptogram validation

--- p8e-api/src/test/kotlin/index/ProtoIndexerTest.kt
+++ p8e-api/src/test/kotlin/index/ProtoIndexerTest.kt
@@ -2,41 +2,39 @@ package index
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.google.common.io.BaseEncoding
-import com.nhaarman.mockitokotlin2.doReturn
+import com.google.protobuf.ByteString
 import helper.TestUtils
 import io.p8e.definition.DefinitionService
-import io.p8e.proto.ContractScope
 import io.p8e.proto.ContractScope.Envelope
 import io.p8e.proto.ContractScope.Scope
 import io.p8e.util.base64Decode
-import io.p8e.util.base64Encode
 import io.p8e.util.toByteString
 import io.p8e.util.toHex
 import io.p8e.util.toProtoUuidProv
-import io.p8e.util.toPublicKeyProto
 import io.provenance.engine.index.ProtoIndexer
 import io.provenance.os.client.OsClient
 import io.provenance.os.domain.inputstream.DIMEInputStream
 import io.provenance.os.domain.inputstream.SignatureInputStream
-import io.provenance.os.util.CertificateUtil
+import io.provenance.p8e.encryption.aes.ProvenanceAESCrypt
 import io.provenance.p8e.encryption.ecies.ECUtils
+import io.provenance.p8e.encryption.ecies.ProvenanceECIESEncrypt
+import io.provenance.p8e.encryption.ecies.ProvenanceKeyGenerator
+import io.provenance.p8e.encryption.kdf.ProvenanceHKDFSHA256
 import io.provenance.p8e.shared.domain.ScopeRecord
 import io.provenance.p8e.shared.domain.ScopeTable
 import io.provenance.proto.encryption.EncryptionProtos
 import io.provenance.proto.encryption.EncryptionProtos.Audience
 import io.provenance.proto.encryption.EncryptionProtos.ContextType.TRANSFER
-import io.provenance.proto.encryption.EncryptionProtos.ContextType.UNKNOWN
 import io.provenance.proto.encryption.EncryptionProtos.Payload
 import org.jetbrains.exposed.dao.id.EntityID
 import org.jetbrains.exposed.sql.SchemaUtils
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.junit.Before
-import org.junit.Ignore
 import org.junit.Test
 import org.mockito.Mockito
 import java.io.InputStream
-import java.nio.charset.Charset
 import java.security.KeyPair
+import java.util.Arrays
 import java.util.UUID
 
 class ProtoIndexerTest {
@@ -91,18 +89,31 @@ class ProtoIndexerTest {
     }
 
     @Test
-    @Ignore
     fun `Validate index fields`(){
         //Setup
         val keyPairs = mapOf(key.public.toHex() to key)
 
-       val dime = EncryptionProtos.DIME.newBuilder()
+        // duplicate the payload tag so cryptogram validation passes.
+        val encryptor = ProvenanceECIESEncrypt(key.public, "")
+        val cryptogram = encryptor.encrypt(BaseEncoding.base64().encode(key.public.encoded).toByteArray(Charsets.UTF_8))
+        val secretKey = ProvenanceKeyGenerator.computeSharedKey(key.private, cryptogram.ephemeralPublicKey)
+        val emphemeralDerivedKey = ProvenanceHKDFSHA256.derive(ECUtils.convertSharedSecretKeyToBytes(secretKey), null, ECUtils.KDF_SIZE)
+
+        val encBytes = Arrays.copyOf(emphemeralDerivedKey, 32)
+        val encKey = ProvenanceAESCrypt.secretKeySpecGenerate(encBytes)
+        val macKeyBytes = Arrays.copyOfRange(emphemeralDerivedKey, 32, 64)
+        val mac = ProvenanceAESCrypt.encrypt(macKeyBytes, "", encKey, true)!!
+
+        val dime = EncryptionProtos.DIME.newBuilder()
             .setUuid(UUID.randomUUID().toProtoUuidProv())
             .setOwner(
                 Audience.newBuilder()
-                    .setPayloadId(123)
-                    .setPublicKey(key.public.toHex().toByteString())
-                    .setContext(UNKNOWN)
+                    .setContext(TRANSFER)
+                    .setEncryptedDek(BaseEncoding.base64().encode(cryptogram.encryptedData).toByteString())
+                    .setEphemeralPubkey(ByteString.copyFrom(BaseEncoding.base64().encode(ECUtils.convertPublicKeyToBytes(cryptogram.ephemeralPublicKey)).toByteArray(Charsets.UTF_8)))
+                    .setPayloadId(1234)
+                    .setTag(ByteString.copyFrom(BaseEncoding.base64().encode(mac).toByteArray(Charsets.UTF_8)))
+                    .setPublicKey(ByteString.copyFrom(BaseEncoding.base64().encode(ECUtils.convertPublicKeyToBytes(key.public)).toByteArray(Charsets.UTF_8)))
                     .build()
             )
             .putMetadata("key", "metadata")
@@ -110,8 +121,9 @@ class ProtoIndexerTest {
                 mutableListOf(
                     Audience.newBuilder()
                         .setContext(TRANSFER)
-                        .setEncryptedDek("something".toByteString())
-                        .setEphemeralPubkey(BaseEncoding.base64().encode(ECUtils.convertPublicKeyToBytes(key.public)).toByteString())
+                        .setTag(ByteString.copyFrom(BaseEncoding.base64().encode(mac).toByteArray(Charsets.UTF_8)))
+                        .setEncryptedDek(BaseEncoding.base64().encode(cryptogram.encryptedData).toByteString())
+                        .setEphemeralPubkey(ByteString.copyFrom(BaseEncoding.base64().encode(ECUtils.convertPublicKeyToBytes(cryptogram.ephemeralPublicKey)).toByteArray(Charsets.UTF_8)))
                         .setPayloadId(1234)
                         .setPublicKey(BaseEncoding.base64().encode(ECUtils.convertPublicKeyToBytes(key.public)).toByteString())
                         .build()
@@ -120,27 +132,12 @@ class ProtoIndexerTest {
             .addAllPayload(
                 mutableListOf(
                     Payload.newBuilder()
-                        .setId(123)
-                        .setCipherText("some-text".toByteString())
+                        .setId(1234)
+                        .setCipherText("".toByteString())
                         .build()
                 )
             )
             .build()
-
-
-        //dimeInputStream.getDecryptedPayload(encryptionKeyPair)
-
-
-        //dimeInputStream.dime
-        //Mockito.`when`(dimeInputStream.dime).thenReturn(dime)
-        //doReturn(dime).`when`(dimeInputStream).dime
-        //Mockito.`when`(osClient.get(envelope.scope.recordGroupList[0].specification.base64Decode(), key.public)).thenReturn(dimeInputStream)
-
-
-//        signatureInputStream = SignatureInputStream(
-//            InputStream.nullInputStream(),
-//
-//        )
 
         dimeInputStream = DIMEInputStream(
             dime = dime,
@@ -148,9 +145,9 @@ class ProtoIndexerTest {
         )
 
         Mockito.`when`(osClient.get(envelope.scope.recordGroupList[0].specification.base64Decode(), key.public)).thenReturn(dimeInputStream)
-       // Mockito.`when`(dimeInputStream.getDecryptedPayload(key)).thenReturn(signatureInputStream)
 
         //Execute
+        //TODO: Running into `Signature is missing from object, has item been fetched from object store?`
         val result = protoIndexer.indexFields(envelope.scope, keyPairs)
 
         //Validate
