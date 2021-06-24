5eb9b7eb60fbb70537faa9466602104f290c77d4
Robert Chaing <rchaing@figure.com>
2021-06-24 09:29:07 -0700

    remove table name

--- p8e-migration/sql/V69__Add_UUID_Key_To_Affiliate.sql
+++ p8e-migration/sql/V69__Add_UUID_Key_To_Affiliate.sql
@@ -1,2 +1,2 @@
 ALTER TABLE affiliate ADD COLUMN signing_key_uuid UUID;
-ALTER TABLE p8e.affiliate ADD COLUMN key_provider_type TEXT NOT NULL DEFAULT 'DATABASE';
+ALTER TABLE affiliate ADD COLUMN key_provider_type TEXT NOT NULL DEFAULT 'DATABASE';
