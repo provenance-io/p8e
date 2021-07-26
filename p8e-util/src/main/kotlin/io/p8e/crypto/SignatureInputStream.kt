7e97e0561760da8a47bab8bb8d0ff68caaa5cb34
Robert Chaing <rchaing@figure.com>
2021-07-26 14:29:00 -0700

    cleanup SignatureInputStream

--- p8e-util/src/main/kotlin/io/p8e/crypto/SignatureInputStream.kt
+++ p8e-util/src/main/kotlin/io/p8e/crypto/SignatureInputStream.kt
@@ -1,23 +1,27 @@
 package io.p8e.crypto
 
+import io.p8e.crypto.SignerType.SIGN
+import io.p8e.crypto.SignerType.VERIFY
 import java.io.InputStream
 import java.security.PublicKey
 import java.security.Signature
 
+enum class SignerType { SIGN, VERIFY }
+data class SignatureRef(val signer: SignerImpl? = null, val verify: Signature? = null, val signerType: SignerType)
+
 class SignatureInputStream(
     private val inputStream: InputStream,
-    private val signer: SignerImpl? = null,
-    private val verify: Signature? = null,
+    private val signatureRef: SignatureRef,
     private val signatureBytes: ByteArray? = null
 ): InputStream() {
     override fun read(): Int {
         val res = inputStream.read()
         if(res != -1) {
-            if(signer != null) {
-                signer.update(inputStream.readAllBytes())
-            } else {
-                verify?.update(inputStream.readAllBytes())
-            }
+           if(signatureRef.signerType == SIGN) {
+               signatureRef.signer?.update(inputStream.readAllBytes())
+           } else {
+               signatureRef.verify?.update(inputStream.readAllBytes())
+           }
         }
         return res
     }
@@ -31,38 +35,39 @@ class SignatureInputStream(
     ): Int {
         val res = inputStream.read(b, off, len)
         if(res != -1) {
-            if(signer != null) {
-                signer.update(b, off, res)
+            if(signatureRef.signerType == SIGN) {
+                signatureRef.signer?.update(b, off, res)
             } else {
-                verify?.update(b, off, res)
+                signatureRef.verify?.update(b, off, res)
             }
         }
         return res
     }
 
     fun sign(): ByteArray {
-        return signer!!.sign()
+        return signatureRef.signer!!.sign()
     }
 
     fun verify(): Boolean {
         if (signatureBytes == null) {
             throw IllegalStateException("Unable to verify signature input stream when in signing mode.")
         }
-        return verify?.verify(signatureBytes)!!
+        return signatureRef.verify!!.verify(signatureBytes)
     }
 }
 
 fun InputStream.sign(signer: SignerImpl): SignatureInputStream {
     return SignatureInputStream(
         inputStream = this,
-        signer = signer.apply { initSign() }
+        signatureRef = SignatureRef(signer.apply{ initSign() }, null, SIGN)
     )
 }
 
 fun InputStream.verify(publicKey: PublicKey, signature: ByteArray): SignatureInputStream {
+    val verifySig = Signature.getInstance(SignerImpl.SIGN_ALGO, SignerImpl.PROVIDER).apply { initVerify(publicKey) }
     return SignatureInputStream(
         inputStream = this,
-        verify = Signature.getInstance(SignerImpl.SIGN_ALGO, SignerImpl.PROVIDER).apply { initVerify(publicKey) },
+        signatureRef = SignatureRef(null, verifySig, VERIFY),
         signatureBytes = signature
     )
 }
